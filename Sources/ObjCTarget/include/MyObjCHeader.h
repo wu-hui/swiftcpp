acf0dfafbb5c59cec08e51290f662c211fbb3ca8
wu-hui <wu.hui.github@gmail.com>
2025-01-17 12:24:28 -0500

    Add objctarget which also depends on cpptarget

---
+++ Sources/ObjCTarget/include/MyObjCHeader.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+#include "Sources/CppTarget/include/CppClassUtil.h"
+
+@interface MyObjCClass : NSObject
+
+- (instancetype)init;
+- (void)callCppMethod;
+
+@end
